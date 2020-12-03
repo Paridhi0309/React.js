ce39a9a4edaf89973459b0f49db8239819c6669c
Paridhi <4paridhi@gmail.com>
2020-12-03 20:15:38 +0530

    components 3

--- practical-react/src/App.js
+++ practical-react/src/App.js
@@ -2,10 +2,12 @@ import React,{Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-const Body=() => (
-  <p className="App-intro">
-    To get started,edit <code>src/App.js</code> and save to reload.
-  </p>
+const Body=props=> (
+  <div>
+  <p className="App-intro">{props.text}</p>
+<p className="App-intro">{props.text2}</p>
+<p className="App-intro">{props.myFunc(1,2)}</p>
+</div>
   );
 
 class Header extends Component{
@@ -14,25 +16,30 @@ class Header extends Component{
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
     <h1 className="App-title">{this.props.title}</h1>
+    <div>{this.props.myFunc(10,12)}</div>
       </header>
-    )
+    );
   }
 }
 
 class App extends Component {
+  add(a,b){
+    return a+b;
+  }
   render (){
     return(
     <div className="App">
-      <Header title="Hello from App"/>
-      <Body/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        </div>
+      <Header title={"Hello"}
+      num={7}
+      myArr={[10,2,3]}
+      myFunc={this.add}
+      myObj={{
+        a:5,
+        b:6
+      }}
+      />
+      <Body myFunc={this.add} text="i am cool" text2="i am cool2"/>
+      </div>
   );
 }
 }
