180f95c815c56f326e3b4b99fd393cec7867c341
Paridhi <4paridhi@gmail.com>
2020-12-06 16:12:29 +0530

    conditionally render

--- practical-react/src/components/Header.js
+++ practical-react/src/components/Header.js
@@ -1,15 +1,12 @@
 import logo from '../logo.svg';
 import React from "react";
-class Header extends React.Component{
+export default class Header extends React.Component{
   render(){
     return(
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
     <h1 className="App-title">{this.props.title}</h1>
-    <div>{this.props.myFunc(10,12)}</div>
       </header>
     );
   }
 }
-
-export default Header;
